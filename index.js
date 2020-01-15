a9555ee5e83175d9a239263b53e41413aad715de
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-01-15 00:13:41 -0300

    Adds connection to database

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config.json');
 
 const app = express();
+
+const { dbUsername, dbPassword, dbClusterUrl, dbName } = config.database;
+mongoose.connect(`mongodb+srv://${dbUsername}:${dbPassword}@${dbClusterUrl}/${dbName}?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
