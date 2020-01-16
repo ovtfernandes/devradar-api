6c639940b2ad0ace408cf0be9be770bb85b11585
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-01-16 03:46:40 -0300

    Adds cors use for allowing cors in api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const config = require('../config.json');
 const routes = require('./routes');
 
@@ -11,6 +12,7 @@ mongoose.connect(`mongodb+srv://${dbUsername}:${dbPassword}@${dbClusterUrl}/${db
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
